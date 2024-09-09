eefbef7a69c35cf7aec79421e14abb31f553fd62
YachaTree <dnftksghkfkd@icloud.com>
2024-09-09 20:03:24 +0900

    feat: modal 추가

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학'])
   let[따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -38,14 +39,13 @@ function App() {
       </div>
 
       <div className="list">
-        <h4>{ 글제목[2] }</h4>
+        <h4 onClick={()=>{setModal(!modal)}}>{ 글제목[2] }</h4>
         <p>2월 7일 발행</p>
       </div>
 
-      <Modal></Modal>
-
-
-
+      {
+        modal == true ? <Modal/> : null
+      }
     </div>
   );
 }
