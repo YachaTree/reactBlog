06aaad4cb85709bf33c16fb7494317024082156d
YachaTree <dnftksghkfkd@icloud.com>
2024-09-10 20:07:35 +0900

    Feat: modal 수정

--- src/App.js
+++ src/App.js
@@ -9,8 +9,12 @@ function App() {
 
   let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학'])
-  let[따봉, 따봉변경] = useState(0);
+  let[따봉, 따봉변경] = useState([0],[0],[0]);
   let [modal, setModal] = useState(false);
+
+  [1,2,3].map(function(a){
+    return '12312'
+  })
 
   return (
     <div className="App">
@@ -22,26 +26,20 @@ function App() {
           글제목변경(copy)
       }}>가나다순 정렬</button>
       
-      <div className="list">
-        <h4>{ 글제목[0] } 
-          <span onClick={() => 
-          {
-            따봉변경(따봉+1)
-          }
-          }>👍</span> {따봉} 
-        </h4>
-        <p>2월 7일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4>{ 글제목[1] }</h4>
-        <p>2월 7일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4 onClick={()=>{setModal(!modal)}}>{ 글제목[2] }</h4>
-        <p>2월 7일 발행</p>
-      </div>
+      {
+        글제목.map(function(a, i){
+          return (
+          <div className="list">
+            <h4 onClick={()=>{setModal(!modal)}}>{ 글제목[i] }</h4>
+            <span onClick={() => 
+            {
+              따봉변경(따봉[i]+1)
+            }
+            }>👍</span> {따봉}
+            <p>2월 7일 발행</p>
+          </div>)
+        })
+      }
 
       {
         modal == true ? <Modal/> : null
